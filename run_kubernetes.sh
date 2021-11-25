09b875f46ae86abcd14a63e5a9cf36d97f0a8d45
Jose Robles Chavez <jrobles@MacBook-Pro-de-Jose.local>
2021-11-24 22:30:12 -0600

    added changes on run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=jrrobles/mlrsbostonprices
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- create deployment ml-rs-boston-prices --image=docker.io/jrrobles/mlrsbostonprices
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl -- get pods
 
 # Step 4:
 # Forward the container port to a host
-
+export POD_NAME=$(minikube kubectl -- get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo POD_NAME: $POD_NAME
+minikube kubectl -- expose deployment/ml-rs-boston-prices --type="NodePort" --port 8000
+minikube kubectl -- port-forward pod/$POD_NAME 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=jrrobles/mlrsbostonprices
+
 
 # Step 2:  
 # Authenticate & tag
+docker login --username jrrobles
+docker tag mlrsbostonprices $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
